922f47ed9a9a395434b85f52183298e2c4c67c65
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-11-19 16:39:05 +0530

    Added:Code for established a connection with Database.

---
+++ src/main/java/Employee_Payroll/EmployeePayroll.java
@@ -0,0 +1,39 @@
+package Employee_Payroll;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
+public class EmployeePayroll {
+
+	public static void main(String[] args) {
+
+		String jdbcURL = "jdbc:mysql://localhost:3306/payrollservice?useSSL=false";
+		String username = "root";
+		String password = "lovey";
+		Connection connection;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("Driver loaded");
+		} catch (ClassNotFoundException classNotFoundException) {
+			throw new IllegalStateException("Cannot find the driver in the classpath: ", classNotFoundException);
+		}
+		listDrivers();
+		try {
+			System.out.println("Connecting to database:" + jdbcURL);
+			connection = DriverManager.getConnection(jdbcURL, username, password);
+			System.out.println("Connection is successful:" + connection);
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+	}
+
+	private static void listDrivers() {
+		Enumeration<Driver> driverList = DriverManager.getDrivers();
+		while (driverList.hasMoreElements()) {
+			Driver driverClass = driverList.nextElement();
+			System.out.println(" " + driverClass.getClass().getName());
+		}
+	}
+}
