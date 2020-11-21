19b900e4afd1c0943ac7c0b365ce9b6b031b4988
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-11-22 04:21:41 +0530

    Added:Code to remove employee from db.

---
+++ src/main/java/Employee_Payroll/EmployeePayrollData.java
@@ -0,0 +1,63 @@
+package Employee_Payroll;
+
+import java.time.LocalDate;
+
+public class EmployeePayrollData {
+
+	public int id;
+	public String name;
+	public double salary;
+	public LocalDate startData;
+	public String department;
+
+	public EmployeePayrollData(int id, String name, double salary, LocalDate startData, String department) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+		this.startData = startData;
+		this.department = department;
+	}
+
+	public EmployeePayrollData(int id, String name, double salary, LocalDate startData) {
+		this(id, name, salary);
+		this.startData = startData;
+	}
+
+	public EmployeePayrollData(int id, String name, double salary) {
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		EmployeePayrollData other = (EmployeePayrollData) obj;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (Double.doubleToLongBits(salary) != Double.doubleToLongBits(other.salary))
+			return false;
+		if (startData == null) {
+			if (other.startData != null)
+				return false;
+		} else if (!startData.equals(other.startData))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "EmployeePayrollData [id=" + id + ", name=" + name + ", salary=" + salary + ", start=" + startData + "]";
+	}
+}
